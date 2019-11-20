e7da94537e997befe1aeaa660fe2c2c4aef6b439
Emine Akgül <49981496+EmineAkgul@users.noreply.github.com>
2019-11-20 09:59:18 +0300

    second_commit

---
+++ NoteBoard/NoteBoard.BLL/PasswordController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoteBoard.DAL.Repositories;
+using NoteBoard.Model;
+
+
+namespace NoteBoard.BLL
+{
+    public class PasswordController
+    {
+        PasswordDAL _passwordDAL;
+        public PasswordController()
+        {
+            _passwordDAL = new PasswordDAL();
+        }
+
+        public bool Add(Password password)
+        {
+            List<Password> passwords = GetAllByUser(password.UserID);
+            passwords = passwords.OrderByDescending(a => a.CreatedDate).Take(3).ToList();
+
+            if (passwords.FirstOrDefault(a => a.PasswordText == password.PasswordText) != null)
+            {
+                throw new Exception("Son 3 Şifreden farklı bir şifre giriniz.!");
+            }
+
+            if(passwords.FirstOrDefault()!=null) //varsa silsin yoksa silmeye çalışıp patlamasın.!
+                Delete(passwords.First());
+
+            password.IsActive = true;
+            password.CreatedDate = DateTime.Now;
+
+            return _passwordDAL.Add(password) > 0;
+        }
+
+        public List<Password> GetAllByUser(int userID)
+        {
+            return _passwordDAL.GetAll().Where(a => a.UserID == userID).ToList();
+        }
+
+        public bool Delete(Password password)
+        {
+            password.IsActive = false;
+            password.ModifiedDate = DateTime.Now;
+            return _passwordDAL.Update(password) > 0;
+        }
+
+        public Password GetActivePassword(int userID)
+        {
+            List<Password> allPasswords = GetAllByUser(userID);
+            return allPasswords.Where(a => a.IsActive).SingleOrDefault();
+        }
+
+    }
+}
--- NoteBoard/NoteBoard.BLL/UserController.cs
+++ NoteBoard/NoteBoard.BLL/UserController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NoteBoard.DAL;
+using NoteBoard.Model;
 
 namespace NoteBoard.BLL
 {
@@ -14,5 +16,85 @@ namespace NoteBoard.BLL
         {
             _userDAL = new UserDAL();
         }
+
+        public bool Add(User user)
+        {
+            try
+            {
+                if (user.Passwords.Count > 0)
+                {
+                    user.IsActive = false;
+                    user.CreatedDate = DateTime.Now;
+                    user.Passwords.First().IsActive = true;
+                    user.Passwords.First().CreatedDate = DateTime.Now;
+
+                    _userDAL.Add(user);
+                    return true;
+                }
+                else
+                {
+                    throw new Exception("Şifre yok.!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public bool Update(User user)
+        {
+            try
+            {
+                user.ModifiedDate = DateTime.Now;
+                _userDAL.Update(user);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public bool Delete(User user)
+        {
+            user.IsActive = false;
+            return Update(user);
+            //hesap 30 günden fazla pasif kalırsa veritabanından silinsin?
+        }
+
+
+        public User GetByID(int userID)
+        {
+            return _userDAL.GetByID(userID);
+        }
+
+
+        public User GetByLogin(string username,string password)
+        {
+            List<User> users = _userDAL.GetAll();
+            User user = users.Where(a => a.IsActive && a.UserName == username).Single();
+            if(user != null)
+            {
+                Password pass = user.Passwords.Where(a => a.IsActive && a.PasswordText == password).Single();
+                if (pass == null)
+                {
+                    return null;
+                }
+            }
+            return user;
+        }
+
+
+        public List<User> GetAll()
+        {
+            return _userDAL.GetAll();
+        }
+
+
     }
 }
